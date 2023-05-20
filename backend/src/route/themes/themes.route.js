23260006d2c2c7365b856cf0cb0d2abd864c2184
diegodurer <110141543+diegodurer@users.noreply.github.com>
2023-05-19 20:33:16 -0400

    Delete backend directory

--- backend/src/route/themes/themes.route.js
+++
@@ -1,8 +0,0 @@
-const themeController = require("../../controller/themes/themes.controller");
-
-module.exports = function (app) {
-    app.get("/themes/list", themeController.listarThemes);
-    app.get("/themes/:id", themeController.buscarPorCodigoThemes);
-    app.post("/themes/update", themeController.actualizarThemes);
-    app.delete("/themes/delete/:id", themeController.eliminarThemes);
-};
--- backend/src/route/topics/topics.route.js
+++
@@ -1,8 +0,0 @@
-const topicController = require("../../controller/topics/topics.controller");
-
-module.exports = function (app) {
-    app.get("/topics/list", topicController.listarTopico);
-    app.get("/topics/:id", topicController.buscarPorCodigoTopico);
-    app.post("/topics/update", topicController.actualizarTopico);
-    app.delete("/topics/delete/:id", topicController.eliminarTopico);
-};
